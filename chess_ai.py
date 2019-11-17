0bf1f24c53e1cd34114f30e181c904e409715ecc
Furkan Emre <furkan54emre@gmail.com>
2019-11-17 19:21:10 +0300

    Completed

--- chess_ai.py
+++ chess_ai.py
@@ -1,6 +1,6 @@
 import chess
 import random
-
+from threading import Lock, Thread
 '''
 	Your Code Will Come Here
 '''
@@ -19,39 +19,127 @@ import random
 
 
 def ai_play(board):
+	# Program works with both minimax_ai_play and alpha_beta_minimax
+	
 	print("Thinking...")
-	minimax_ai_play(board)
+	# play = minimax_ai_play(board, 5, get_turn(board))
+	play = alpha_beta_minimax(board, 5, negative_infinity(), positive_infinity(), get_turn(board))[1]
 	print("Thinking Finished...")
-	return "g1g2"
+	
+	print("Played move:" + play)
+	return play
 
 def get_turn(board):
 	if(board.fen().split(" ")[1] == "w"):
 		return True
 	return False
 
-def minimax_ai_play(board):
-	max_val = 0
+def minimax_ai_play(board, depth, maximizingPlayer):
+	# This approach prefered because I can easily split work to 
+	# different threads
 
+	max_val = negative_infinity()
+	min_val = positive_infinity()
+
+	index = 0
+	
+	candidate_moves = list(board.legal_moves)
+
+	# includes values heuristic values for play0 in 0th index
+	moves_calculated = []
+
+	count = 0
 	for play in board.legal_moves:
 		play_move(board, play)
-		
-		value = minimax(board, 4, get_turn(board))
-	
+		# print("---------------------")
+		# print(play)
+		value = minimax(board, depth - 1, get_turn(board))
+		# print(value)
+		# print("---------------------")
 		unmake_last_move(board)
-	
+
+		if(maximizingPlayer):
+			if(value > max_val):
+				max_val = value
+				index = count
+		else:
+			if(value < min_val):
+				min_val = value
+				index = count
+			
+		moves_calculated.append(value)
+				
+		count += 1
+
+	# Randomizing the best alternatives
+	choosen = []
+	for move in range(len(moves_calculated)):
+		if(moves_calculated[index] == moves_calculated[move]):
+			choosen.append(move)
+
+	index = choosen[random.randint(0, len(choosen) - 1)]
+
+	return str(candidate_moves[index])
+
 def play_move(board, move):
 	move_to_play = chess.Move.from_uci(str(move))
 	if(board.is_legal(move_to_play)):
 		board.push(move_to_play)
+		return str(move)
 	else:
 		raise ValueError("Illegal Move")
 
 def unmake_last_move(board):
 	return board.pop()
 
+def alpha_beta_minimax(board, depth, alpha, beta, maximizingPlayer):
+	# It can work faster with proper move ordering
+	if(depth == 0 or is_terminal_node(board)):
+		return heuristic_value(board, depth), -1
+	if(maximizingPlayer):
+		value = negative_infinity()
+		move = 0
+
+		for play in board.legal_moves:
+			candidate_move = play_move(board, play) # child board
+
+			candidate_value = alpha_beta_minimax(board, depth - 1, alpha, beta, get_turn(board))[0]
+			if(value < candidate_value):
+				value = candidate_value
+				move = candidate_move
+			
+
+			# value = max(value, alpha_beta_minimax(board, depth - 1, alpha, beta, get_turn(board)))
+			unmake_last_move(board)
+
+			alpha = max(alpha, value)
+			if(alpha >= beta):
+				break # beta cut-off
+		return value, move
+	else:
+		value = positive_infinity()
+		move = 0
+
+		for play in board.legal_moves:
+			candidate_move = play_move(board, play) # child board
+
+			candidate_value = alpha_beta_minimax(board, depth - 1, alpha, beta, get_turn(board))[0]
+			if(value > candidate_value):
+				value = candidate_value
+				move = candidate_move
+
+
+			# value = min(value, alpha_beta_minimax(board, depth - 1, alpha, beta, get_turn(board)))
+			unmake_last_move(board)
+
+			beta = min(beta, value)
+			if(alpha >= beta):
+				break # alpha cut off
+		return value, move
+
 def minimax(board, depth, maximizingPlayer):
 	if(depth == 0 or is_terminal_node(board)):
-		return heuristic_value(board)
+		return heuristic_value(board, depth)
 	if(maximizingPlayer):
 		value = negative_infinity()
 		
@@ -74,8 +162,47 @@ def is_terminal_node(board):
 		return True
 	return False
 
-def heuristic_value(board):
-	return random.randint(5, 200)
+def heuristic_value(board, depth):
+	board_state = board.fen().split()[0]
+
+	pieces = ["Q", "R", "B", "N", "P"]
+	coefficents = [900, 500, 300, 300, 100]
+	
+	heuristic_result = 0.0
+
+	for piece, coefficent in zip(pieces, coefficents):
+		white_count = board_state.count(piece)
+		black_count = board_state.count(piece.lower())
+
+		heuristic_result += coefficent * (white_count -  black_count)
+
+	if(board.is_checkmate()):
+		if(get_turn(board)):
+			heuristic_result -= (20000 + 2 ** depth)
+		else:
+			heuristic_result += (20000 + 2 ** depth)
+
+	
+	heuristic_result += 1 * calculate_mobilility(board)
+	return heuristic_result
+
+def calculate_mobilility(board):
+	white_mobility = 0
+	black_mobility = 0
+	if(get_turn(board)):
+		# White Turn
+		white_mobility = board.legal_moves.count()
+		board.turn = chess.BLACK
+		black_mobility = board.legal_moves.count()
+		board.turn = chess.WHITE
+	else:
+		# Black Turn
+		black_mobility = board.legal_moves.count()
+		board.turn = chess.WHITE
+		white_mobility = board.legal_moves.count()
+		board.turn = chess.BLACK
+		
+	return white_mobility - black_mobility
 
 def positive_infinity():
 	return float("inf")
--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import chess
 
 if __name__ == '__main__':
 	app = QApplication(sys.argv)
-	# board = chess.Board("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1") #initial board is created here
-	board = chess.Board("8/5K1k/8/8/8/8/8/6Q1 w - - 0 1")
+	board = chess.Board("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1") #initial board is created here
+	# board = chess.Board("3K2R1/n3r3/5P1P/8/n7/pp6/kp6/3Q4 b - - 0 1")
 	ex = App(board)
 	app.exec_()
